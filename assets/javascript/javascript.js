63714d8b2714f4eda1bcc48681bdd161895f6976
Damon Foster <Damon Foster>
2016-09-17 13:40:34 -0500

    on click function and basic ajax call to NR API

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,3 +3,21 @@
 //Ajax call to Food2Fork API
 //JQuery to add movie suggestion to HTML
 //JQuery to add recipes to HTML
+
+console.log('testing 1 2');
+
+$('.submit').on('click', function(){
+	var queryURL = "http://netflixroulette.net/api/api.php?title=Attack%20on%20titan";
+	//default call from API website. parameters will be changed based on user input
+
+	console.log('tester');
+
+	$.ajax({
+		url: queryURL,
+		method: 'GET'
+	}).done( function(movie) {
+		console.log(movie);
+	})
+})
+
+
